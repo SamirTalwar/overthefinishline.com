887d8334cb71d1f99e0861210f245c013db3992c
Samir Talwar <samir@noodlesandwich.com>
2016-05-12 16:24:54 +0100

    API: Start a web server on port 8002.

--- api/app/Main.hs
+++ api/app/Main.hs
@@ -1,6 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib
+import Data.Monoid (mconcat)
+import System.Environment (getArgs)
+import Web.Scotty
 
-main :: IO ()
-main = someFunc
+main = do
+  port <- read . head <$> getArgs
+  scotty port $
+    get "/:word" $ do
+      beam <- param "word"
+      html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]

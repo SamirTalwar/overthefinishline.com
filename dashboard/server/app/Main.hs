8f8c42c997ceb710f12ccfab44409baa98a34e4a
Samir Talwar <samir@noodlesandwich.com>
2016-05-13 13:05:35 +0100

    Dashboard: Serve files from the Haskell server.

--- dashboard/server/app/Main.hs
+++ dashboard/server/app/Main.hs
@@ -1,6 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib
+import Prelude hiding (readFile)
+import Data.Monoid (mconcat)
+import Network.Wai.Middleware.Static (addBase, noDots, staticPolicy, (>->))
+import Options.Applicative
+import System.Environment (getArgs)
+import System.FilePath
+import Web.Scotty
 
-main :: IO ()
-main = someFunc
+type Port = Int
+data ServerOptions = ServerOptions { clientPath :: FilePath, port :: Port }
+
+main = execParser parseOptions >>= server
+
+server (ServerOptions clientPath port) =
+  scotty port $ do
+    middleware $ staticPolicy (noDots >-> addBase clientPath)
+    get "/" $ file (clientPath </> "index.html")
+
+parseOptions =
+  info (helper <*> parser) fullDesc
+  where
+    parser = ServerOptions
+      <$> strOption (long "client" <> metavar "CLIENT-PATH" <> help "Path to the client files")
+      <*> option auto (long "port" <> metavar "PORT" <> help "Server port")

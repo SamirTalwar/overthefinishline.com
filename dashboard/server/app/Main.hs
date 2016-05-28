8a317469914bc1becb5991a33ba8d6c96c647506
Samir Talwar <samir@noodlesandwich.com>
2016-05-28 15:20:43 +0100

    Dashboard: Read configuration from environment variables.

--- dashboard/server/app/Main.hs
+++ dashboard/server/app/Main.hs
@@ -5,24 +5,21 @@ module Main where
 import Prelude hiding (readFile)
 import Data.Monoid (mconcat)
 import Network.Wai.Middleware.Static (addBase, noDots, staticPolicy, (>->))
-import Options.Applicative
-import System.Environment (getArgs)
+import System.Environment (getEnv)
 import System.FilePath
 import Web.Scotty
 
 type Port = Int
-data ServerOptions = ServerOptions { clientPath :: FilePath, port :: Port }
+data ServerOptions = ServerOptions { port :: Port, clientPath :: FilePath }
 
-main = execParser parseOptions >>= server
+main = readOptions >>= server
 
-server (ServerOptions clientPath port) =
+server (ServerOptions port clientPath) =
   scotty port $ do
     middleware $ staticPolicy (noDots >-> addBase clientPath)
     get "/" $ file (clientPath </> "index.html")
 
-parseOptions =
-  info (helper <*> parser) fullDesc
-  where
-    parser = ServerOptions
-      <$> strOption (long "client" <> metavar "CLIENT-PATH" <> help "Path to the client files")
-      <*> option auto (long "port" <> metavar "PORT" <> help "Server port")
+readOptions =
+  ServerOptions
+   <$> (read <$> getEnv "PORT")
+   <*> getEnv "CLIENT_PATH"

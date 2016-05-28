e96daaf93f6b11d69d45b00510ac3dae4ae983fc
Samir Talwar <samir@noodlesandwich.com>
2016-05-28 23:43:38 +0100

    Dashboard: Authenticate against GitHub with OAuth.

--- dashboard/client/src/Page/Authentication.elm
+++ dashboard/client/src/Page/Authentication.elm
@@ -5,7 +5,7 @@ import Html.Attributes exposing (class, href)
 
 html =
   [
-    a [class "sign-in", href "/authenticate"] [
+    a [class "sign-in", href "/authentication/by/github"] [
       span [class "fa fa-github"] [],
       text "Sign in with GitHub"
     ]
--- dashboard/server/app/Main.hs
+++ dashboard/server/app/Main.hs
@@ -2,24 +2,70 @@
 
 module Main where
 
-import Prelude hiding (readFile)
-import Data.Monoid (mconcat)
+import Control.Monad.IO.Class (liftIO)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as ByteString
+import qualified Data.ByteString.Char8 as ByteStringC
+import Data.ByteString.Lazy (fromStrict, toStrict)
+import Data.Text.Lazy (pack)
+import Data.Text.Lazy.Encoding (decodeUtf8, encodeUtf8)
+import Network.HTTP.Client (newManager)
+import Network.HTTP.Client.TLS (tlsManagerSettings)
+import Network.OAuth.OAuth2
 import Network.Wai.Middleware.Static (addBase, noDots, staticPolicy, (>->))
 import System.Environment (getEnv)
 import System.FilePath
+import System.IO.Error
 import Web.Scotty
 
 type Port = Int
-data Configuration = Configuration { port :: Port, clientPath :: FilePath }
+data OAuthCredentials = OAuthCredentials {
+  clientId :: ByteString,
+  clientSecret :: ByteString
+}
+data Configuration = Configuration {
+  port :: Port,
+  clientPath :: FilePath,
+  gitHubOAuthCredentials :: OAuthCredentials
+}
 
 main = readConfiguration >>= server
 
-server (Configuration port clientPath) =
+server (Configuration port clientPath (OAuthCredentials gitHubClientId gitHubClientSecret)) = do
+  httpManager <- newManager tlsManagerSettings
   scotty port $ do
     middleware $ staticPolicy (noDots >-> addBase clientPath)
     get "/" $ file (clientPath </> "index.html")
+    get "/authentication/by/github" $
+      redirect $ decodeUtf8 $ fromStrict $ authorizationUrl gitHubOAuth
+    get "/authorization/by/github" $ do
+      code <- param "code"
+      response <- liftIO $ fetchAccessToken httpManager gitHubOAuth (toStrict $ encodeUtf8 code)
+      case response of
+        Left failure -> text $ decodeUtf8 failure
+        Right accessToken -> text $ pack $ show accessToken
+  where
+    gitHubOAuth = OAuth2 {
+      oauthClientId = gitHubClientId,
+      oauthClientSecret = gitHubClientSecret,
+      oauthOAuthorizeEndpoint = "https://github.com/login/oauth/authorize?scope=user:email%20repo",
+      oauthAccessTokenEndpoint = "https://github.com/login/oauth/access_token",
+      oauthCallback = Nothing
+    }
 
 readConfiguration =
   Configuration
-   <$> (read <$> getEnv "PORT")
+   <$> readEnv "PORT"
    <*> getEnv "CLIENT_PATH"
+   <*> (OAuthCredentials
+     <$> byteStringEnv "GITHUB_OAUTH_CLIENT_ID"
+     <*> byteStringEnv "GITHUB_OAUTH_CLIENT_SECRET")
+  where
+    byteStringEnv name = ByteStringC.pack <$> getEnv name
+
+    readEnv :: Read a => String -> IO a
+    readEnv name = do
+      value <- getEnv name
+      readIO value `catchIOError` \theError -> do
+        putStrLn $ "Failed to parse the variable " ++ name ++ " with the value \"" ++ value ++ "\"."
+        ioError theError

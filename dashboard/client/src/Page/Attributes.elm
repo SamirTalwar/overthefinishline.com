ed7e8b856acbea3e2b92e91c9c9d291ca04d5563
Samir Talwar <samir@noodlesandwich.com>
2016-08-16 08:04:59 +0100

    Dashboard: Use `Url.toString` in one place.

--- dashboard/client/src/Page/Attributes.elm
+++ dashboard/client/src/Page/Attributes.elm
@@ -4,6 +4,16 @@ import Html
 import Html.Attributes exposing (attribute, property)
 import Json.Encode as Json
 import String
+import Url exposing (Url)
+
+href : Url -> Html.Attribute a
+href = Url.toString >> Html.Attributes.href
+
+emptyHref : Html.Attribute a
+emptyHref = Html.Attributes.href "#"
+
+src : Url -> Html.Attribute a
+src = Url.toString >> Html.Attributes.src
 
 role : String -> Html.Attribute a
 role value = property "role" (Json.string value)
--- dashboard/client/src/Page/Navigation.elm
+++ dashboard/client/src/Page/Navigation.elm
@@ -1,7 +1,7 @@
 module Page.Navigation exposing (html)
 
 import Html exposing (..)
-import Html.Attributes exposing (..)
+import Html.Attributes exposing (alt, class, style)
 import Html.Events exposing (..)
 import Page.Attributes exposing (..)
 import Url
@@ -12,16 +12,16 @@ import Navigation exposing (..)
 html : Me -> State -> Html Navigation.Message
 html (Me (User username avatar) projects) (ProjectsShown projectsShown) =
   nav [class "navbar navbar-dark bg-inverse"] [
-    a [class "navbar-brand", href "/"] [text "Over The Finish Line"],
+    a [class "navbar-brand", href (Url.parse "/")] [text "Over The Finish Line"],
     ul [class "nav navbar-nav"]  [
       li [class "nav-item"] [
-        a [class "nav-link", href "#"] [
-          span [class "avatar"] [img [src (avatarLink avatar |> Url.toString), alt ""] []],
+        a [class "nav-link", emptyHref] [
+          span [class "avatar"] [img [src (avatarLink avatar), alt ""] []],
           span [class "username"] [text username]
         ]
       ],
       li [class "nav-item dropdown", onMouseEnter (ProjectsShown True), onMouseLeave (ProjectsShown False)] [
-        a [class "nav-link dropdown-toggle", role "button", ariaHaspopup True, ariaExpanded projectsShown, href "#"] [
+        a [class "nav-link dropdown-toggle", role "button", ariaHaspopup True, ariaExpanded projectsShown, emptyHref] [
           text "Projects"
         ],
         div [class "dropdown-menu", style [("display", if projectsShown then "block" else "none")]] (
@@ -29,7 +29,7 @@ html (Me (User username avatar) projects) (ProjectsShown projectsShown) =
             [span [class "dropdown-item"] [text "You have no projects."]]
           else
             (projects |> List.map (\(Project name url) ->
-              a [class "dropdown-item", href (Url.toString url)] [text name]
+              a [class "dropdown-item", href url] [text name]
             ))
         )
       ]

82a2d2d8ffe381a2bb37f1a48c6909366255d3cd
Samir Talwar <samir@noodlesandwich.com>
2016-08-21 01:35:03 +0100

    Dashboard: Pluralise correctly.

--- dashboard/client/src/App/Page/Dashboard.elm
+++ dashboard/client/src/App/Page/Dashboard.elm
@@ -10,34 +10,37 @@ import App.Model exposing (..)
 
 html : Dashboard -> List (Html Message)
 html (Dashboard _ now pullRequests) =
-  [
-    section [class "summary"] [
-      span [] [text "You have"],
-      span [class "display-3"] [text (toString <| List.length pullRequests)],
-      span [] [text "open pull requests."]
-    ],
-    section [class "details"] [
-      table [class "table"] (
-        pullRequests
-          |> List.take 3
-          |> List.map (\pullRequest ->
-            let
-              old = Moment.durationBetween pullRequest.updatedAt now > Moment.durationOf 3 Moment.Hours
-            in
-              tr [] [
-                td [class "link"] [
-                  a [href pullRequest.repository.url] [
-                    text (pullRequest.repository.owner ++ " / " ++ pullRequest.repository.name)]],
-                td [class "link"] [
-                  a [href pullRequest.url] [
-                    text ("#" ++ (toString pullRequest.number))]],
-                td [class "link"] [
-                  a [href pullRequest.url] [
-                    text pullRequest.title]],
-                td (if old then [class "old"] else []) [
-                  text ("updated " ++ pullRequest.updatedAt `Moment.from` now)]
-              ]
-          )
-      )
+  let
+    pullRequestCount = List.length pullRequests
+  in
+    [
+      section [class "summary"] [
+        span [] [text "You have"],
+        span [class "display-3"] [text (toString pullRequestCount)],
+        span [] [text (if pullRequestCount == 1 then "open pull request." else "open pull requests.")]
+      ],
+      section [class "details"] [
+        table [class "table"] (
+          pullRequests
+            |> List.take 3
+            |> List.map (\pullRequest ->
+              let
+                old = Moment.durationBetween pullRequest.updatedAt now > Moment.durationOf 3 Moment.Hours
+              in
+                tr [] [
+                  td [class "link"] [
+                    a [href pullRequest.repository.url] [
+                      text (pullRequest.repository.owner ++ " / " ++ pullRequest.repository.name)]],
+                  td [class "link"] [
+                    a [href pullRequest.url] [
+                      text ("#" ++ (toString pullRequest.number))]],
+                  td [class "link"] [
+                    a [href pullRequest.url] [
+                      text pullRequest.title]],
+                  td (if old then [class "old"] else []) [
+                    text ("updated " ++ pullRequest.updatedAt `Moment.from` now)]
+                ]
+            )
+        )
+      ]
     ]
-  ]
